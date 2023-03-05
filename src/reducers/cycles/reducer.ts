388c552058c406ce43e025db8c11e2f07a47fabe
Alley M. Carvalho <alleycarvalho@gmail.com>
2023-03-05 03:11:15 -0300

    feat: add reducer and actions

---
+++ src/reducers/cycles/reducer.ts
@@ -0,0 +1,55 @@
+import { ActionTypes } from './actions'
+
+export interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptDate?: Date
+  finishedDate?: Date
+}
+
+interface CyclesState {
+  activeCycleId: string | null
+  cycles: Cycle[]
+}
+
+export function cyclesReducer(state: CyclesState, action: any) {
+  switch (action.type) {
+    case ActionTypes.ADD_NEW_CYCLE:
+      return {
+        ...state,
+        activeCycleId: action.payload.newCycle.id,
+        cycles: [...state.cycles, action.payload.newCycle],
+      }
+
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
+      return {
+        ...state,
+        activeCycleId: null,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, interruptDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+      }
+
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
+      return {
+        ...state,
+        activeCycleId: null,
+        cycles: state.cycles.map((cycle) => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, finishedDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+      }
+
+    default:
+      return state
+  }
+}
