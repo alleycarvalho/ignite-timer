64c122fb6d841dc31204912843d5588a6e0527dd
Alley M. Carvalho <alleycarvalho@gmail.com>
2023-03-05 01:49:01 -0300

    feat(home): add reset to the form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -35,14 +35,19 @@ export function Home() {
     },
   })
 
-  const { handleSubmit, /* reset, */ watch } = newCycleForm
+  const { handleSubmit, reset, watch } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
 
   const task = watch('task')
   const isSubmitDisabled = !task
 
   return (
     <HomeContainer>
-      <form action="" onSubmit={handleSubmit(createNewCycle)}>
+      <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormProvider {...newCycleForm}>
           <NewCycleForm />
         </FormProvider>
